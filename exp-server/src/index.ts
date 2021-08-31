cc11b363c1a1bee989e4f0be5aedba65384191b5
Dar Dualba <ddualba@gmail.com>
2021-08-31 11:26:20 -0700

    finished express + TS integration, and decorators

---
+++ exp-server/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(cookieSession({ keys: ['lafdsdf'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
